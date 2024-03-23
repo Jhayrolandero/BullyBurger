c32194ae955e3384272679bc79fc33fb4c57e179
Cjay Landero <xjaylandero@gmail.com>
2024-03-23 22:57:09 +0800

    added sec-2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ var burgerPics = ["./"];
 const displayedBurger = document.getElementById("featured-burger");
 
 const displayBurger = (name) => {
+  console.log("WOrking here");
   displayedBurger.src = name;
 
   // displayedBurger.classList.remove("burgerAnimation");
@@ -15,4 +16,17 @@ const displayBurger = (name) => {
   }, 3000); // Adjust timeout to match animation duration};
 };
 
-customElements.define("main-nav", Nav);
+const chooseBurger1 = document.getElementById("burger-1");
+
+chooseBurger1.addEventListener("click", () => {
+  displayBurger("./public/images/burgers/burger-1.svg");
+});
+
+const chooseBurger2 = document.getElementById("burger-2");
+chooseBurger2.addEventListener("click", () => {
+  displayBurger("./public/images/burgers/burger-2.svg");
+});
+const chooseBurger3 = document.getElementById("burger-3");
+chooseBurger3.addEventListener("click", () => {
+  displayBurger("./public/images/burgers/burger-3.svg");
+});
